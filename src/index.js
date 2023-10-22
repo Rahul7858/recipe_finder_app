7e5fc17a0c4d328e9b596388a875445084a06a36
Rahul7858 <aman956727@gmail.com>
2023-10-22 18:26:27 +0530

    3 first commit

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter >
+  < HashRouter>
     <Provider store={store}>
       <App />
       <Toaster/>
     </Provider>
-  </BrowserRouter>
+  </ HashRouter>
 );
